46e2fa4d0b601b9522d4e5c90f61251fe327d10f
Adam Henry <adamhenry@adam-henrys-computer.local>
2008-05-16 18:52:52 +0800

    Added mining

--- app/controllers/game_play_controller.rb
+++ app/controllers/game_play_controller.rb
@@ -1,3 +1,13 @@
 class GamePlayController < ApplicationController
-	@ship = Ship.new :name => "Infinity", :max_cargo => 300, :cargo => 0
+	def index
+		@ship = Ship.find 1
+	end
+	
+	def mine_astroid
+		@ship = Ship.find 1
+		@ship.cargo += (ore = rand(15) + 1)
+		@ship.save
+		flash[:notice] = "You have mined #{ore} tons of ore form the Astroid"
+		redirect_to (:action => :index)
+	end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,22 +1,17 @@
 class Ship < ActiveRecord::Base
+	validates_presence_of :name
+	validates_numericality_of :cargo, :max_cargo
 
-	def add_cargo c
-		if @cargo + c < @max_cargo 
-			@cargo = @max_cargo
-		else
-			return false
-		end
+	def full?
+		cargo >= max_cargo ? true : false
 	end
 
-	def cargo_space
-		@max_cargo - @cargo
+	def cargo_room
+		max_cargo - cargo
 	end
-	
-	def cargo_space?
-		( @spcae = cargo_space ) != 0 ? @space : false
-	end
-	
-	def full
-		@cargo == @max_cargo ? true : false
+
+	protected
+	def validate
+		raise( "Cargo hold full for -#{name}") if cargo > max_cargo
 	end
 end
